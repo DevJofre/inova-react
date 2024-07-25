74ccfa86c4a76da23616812cf8f3ab52aac6e981
Jofre Tomas <jofretomas9@gmail.com>
2024-07-25 17:58:26 -0300

    video da home add e pequenas configuração de costumização

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import './home.css'
+import video from '../../Assets/video2.mp4'
 
 const Home = () => {
     return (
         <section className="home">
             <div className="overlay"></div>
-            <video src=""></video>
+            <video src={video} muted autoPlay loop type="video/mp4" ></video>
+
+            <div className="homeContent container">
+                <div className="textDiv">
+                    <span className="smallText">
+                        Nossos Produtos
+                    </span>
+                    <h1 className="homeTitle">
+                        Escolha seus Produtos
+                    </h1>
+                </div>
+            </div>
         </section>
+
     )
 }
 
