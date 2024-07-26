416e8e328cf3257010eb2ef5b11fc821ea634e99
Jofre Tomas <jofretomas9@gmail.com>
2024-07-26 15:55:35 -0300

    home mobile finalizada

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import './home.css'
 import video from '../../Assets/video2.mp4'
+import { FaSearch } from "react-icons/fa";
+import { SlSocialFacebook, SlSocialInstagram, SlSocialLinkedin } from "react-icons/sl";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+
 
 const Home = () => {
     return (
@@ -16,6 +21,34 @@ const Home = () => {
                     <h1 className="homeTitle">
                         Escolha seus Produtos
                     </h1>
+                </div>
+
+                <div className="cardDiv grid">
+                    <div className="destinationInput">
+                        <label htmlFor="product">
+                            Procure seu Produtos:
+                        </label>
+
+                        <div className="input flex">
+                            <input type="text" placeholder="Digite o nome aqui..." />
+                            <FaSearch className="icon"  />
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="homeFooterIcons flex">
+
+                    <div className="rightIcons">
+                        <SlSocialFacebook className="icon" />
+                        <SlSocialInstagram className="icon" />
+                        <SlSocialLinkedin className="icon" />
+                    </div>
+
+                    <div className="leftIcons">
+                        <BsListTask className="icon" />
+                        <TbApps className="icon" />
+                    </div>
                 </div>
             </div>
         </section>
